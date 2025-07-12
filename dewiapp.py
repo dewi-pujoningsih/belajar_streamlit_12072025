983361775677ea0bb7816a906c3f1e93a21d489a
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-12 08:43:23 +0700

    Update dewiapp.py

--- dewiapp.py
+++ dewiapp.py
@@ -1,3 +1,9 @@
 import streamlit as st
-
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
 st.write("Hello, *World!* :sunglasses:")
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
