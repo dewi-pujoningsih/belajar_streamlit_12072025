01021de8a0574646678cda3dc01768837b452df2
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-12 09:23:52 +0700

    Update dewiapp.py

--- dewiapp.py
+++ dewiapp.py
@@ -29,3 +29,15 @@ with col2:
 with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
