a1a7cd7c3e6bf25dadc05dffa652df1600ae444a
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-12 09:07:51 +0700

    Update dewiapp.py

--- dewiapp.py
+++ dewiapp.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
@@ -7,3 +10,8 @@ st.markdown("*Streamlit* is **really** ***cool***.")
 st.markdown('''
     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
     :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+
+st.dataframe(df)
